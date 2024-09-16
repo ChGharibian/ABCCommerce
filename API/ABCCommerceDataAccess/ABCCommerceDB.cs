e1be4caf8552b47254f067840782c04de8615cbe
TimberStalker <timberstalkervideos@gmail.com>
2024-09-16 16:23:51 -0700

    added data access project

---
+++ API/ABCCommerceDataAccess/ABCCommerceDB.cs
@@ -0,0 +1,21 @@
+﻿using ABCCommerceDataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCCommerceDataAccess;
+public class ABCCommerceDB : DbContext
+{
+    public ABCCommerceDB()
+    {
+    }
+
+    public ABCCommerceDB(DbContextOptions<ABCCommerceDB> options)
+        : base(options)
+    {
+    }
+    DbSet<User> Users { get; set; }
+}
