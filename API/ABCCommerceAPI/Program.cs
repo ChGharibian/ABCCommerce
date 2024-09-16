57facd286e714fe5ba4acfa016bdeee8ffec9821
TimberStalker <timberstalkervideos@gmail.com>
2024-09-16 16:33:19 -0700

    Added sql server reference to api

--- API/ABCCommerceAPI/Program.cs
+++ API/ABCCommerceAPI/Program.cs
@@ -1,4 +1,11 @@
+using ABCCommerceDataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ABCCommerceDB>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ABC")));
+builder.Services.AddLogging();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
