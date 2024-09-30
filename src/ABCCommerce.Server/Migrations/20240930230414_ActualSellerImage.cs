f7943bb205decdadbcfda208ef7f12dd532825d6
TimberStalker <timberstalkervideos@gmail.com>
2024-09-30 16:07:24 -0700

    Migrations

---
+++ src/ABCCommerce.Server/Migrations/20240930230414_ActualSellerImage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ABCCommerce.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualSellerImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Sellers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Sellers");
+        }
+    }
+}
