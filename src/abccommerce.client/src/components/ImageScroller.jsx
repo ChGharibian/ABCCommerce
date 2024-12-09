02a707c7c634f5622c1f4b20b8578c75af203c8e
thomas-scott6 <thomas.scott.837@my.csun.edu>
2024-12-08 23:57:47 -0800

    added ability to remove/add images on editlisting page

--- src/abccommerce.client/src/components/ImageScroller.jsx
+++ src/abccommerce.client/src/components/ImageScroller.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Arrow from "./Arrow";
 import './ImageScroller.css';
 import { ArrayUtil } from "../util/arrays";
+import { FileUtil } from "../util/files";
 /**
  * @category component
  * @function ImageScroller
@@ -12,30 +13,51 @@ import { ArrayUtil } from "../util/arrays";
  * @param {Array<String>} props.images List of image sources to display
  * @returns {JSX.Element}
  */
-export default function ImageScroller({images}) {
+export default function ImageScroller({images, handleImageChange = () => {}, onActiveImages = () => {}}) {
     const [currentImage, setCurrentImage] = useState(0);
+    const [activeImages, setActiveImages] = useState([]);
 
-    const scrollLeft = () => {
-        setCurrentImage(ArrayUtil.getInBoundIndex(images, currentImage - 1));
+    useEffect(() => {
+        getActiveImages();
+    }, [images])
+
+    const getActiveImages = async () => {
+        let validList = [];
+        for(let i = 0; i < images.length; i++) {
+            if(await FileUtil.isValidImgSrc(images[i])) validList.push({
+                src: images[i],
+                realIndex: i
+            })
+        }
+        setActiveImages(validList);
+        onActiveImages(validList);
     }
 
-    const scrollRight = () => {
-        setCurrentImage(ArrayUtil.getInBoundIndex(images, currentImage + 1));
+    const scrollLeft = async () => {
+        let index = ArrayUtil.getInBoundIndex(activeImages, currentImage - 1);
+        setCurrentImage(index);
+        handleImageChange(activeImages[index].realIndex);
+    }
+
+    const scrollRight = async () => {
+        let index = ArrayUtil.getInBoundIndex(activeImages, currentImage + 1);
+        setCurrentImage(index);
+        handleImageChange(activeImages[index].realIndex);
     }
 
     return (
         <div className="image-scroller-wrapper">
-            {images.length > 1 &&
+            {activeImages.length > 1 &&
             <div className="image-scroller-left-controls" onClick={scrollLeft}>
                 <Arrow size={13} thickness={3} direction="left" zIndex={2000} />
             </div>
             }
-            {images.length > 0 ?
-            <img className="image-scroller-image" src={images[currentImage]} />
+            {activeImages.length > 0 ?
+            <img className="image-scroller-image" src={activeImages[currentImage].src} alt="Deleted" />
             :
             "No images"
             }
-            {images.length > 1 &&
+            {activeImages.length > 1 &&
             <div className="image-scroller-right-controls" onClick={scrollRight} >
                 <Arrow size={13} thickness={3} direction="right" zIndex={2000} />
             </div>
